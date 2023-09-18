5079d51fc0e07ce929996ee40422704b44deeb6d
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-18 17:32:41 +0200

    Add enemy, entity, player and dictionary class

---
+++ src/entity/entity.js
@@ -0,0 +1,10 @@
+class Entity {
+    constructor(gameScreen, left, top, height, width) {
+        this.sprite;
+        this.state;
+        this.positionX;
+        this.positionY;
+        this.health;
+        this.dictionary;
+    }
+}
