a65036820b74948161356f99dffd2203042083f5
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-18 19:04:31 +0200

    Add Main Meaning class

---
+++ src/entity/dictionary/words/meaning/mainMeaning.js
@@ -0,0 +1,8 @@
+class MainMeaning extends Meaning {
+    constructor(meaning) {
+        this.meaning = meaning;
+        this.damage = 5;
+        this.effect = "fail";
+        this.sprite = "Fail/Sprite/path";
+    }
+}
