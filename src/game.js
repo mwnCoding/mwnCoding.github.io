22f2a9de2248c46a0c92151e4221e1389f14363f
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-18 16:46:54 +0200

    setup constructor of Game class

--- src/game.js
+++ src/game.js
@@ -5,6 +5,15 @@ class Game {
         this.instructionsScreen = document.getElementById('instructions-screen');
         this.optionsScreen = document.getElementById('options-screen');
         this.highscoresScreen = document.getElementById('highscores-screen');
-        
+        this.height = 600;
+        this.width = 500;
+        this.wave = 0;
+        this.time = 0;
+        //TODO: Add player after class definition
+        this.player;
+        //TODO: Add enemies array after class definition
+        this.enemies;
+        //TODO: Select first enemy after class definition
+        this.target;
     }
 }
