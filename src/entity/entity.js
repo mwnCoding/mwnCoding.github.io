aa269b4eb31d3a5263e48abfa1d042cda37edb95
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-18 17:55:29 +0200

    Initialized Entity class

--- src/entity/entity.js
+++ src/entity/entity.js
@@ -1,10 +1,49 @@
 class Entity {
-    constructor(gameScreen, left, top, height, width) {
-        this.sprite;
+    constructor(gameScreen, left, top, height, width, health, dictionary) {
+        this.gameScreen = gameScreen;
+        this.positionX = left;
+        this.positionY = top;
+        this.width = width;
+        this.height = height;
         this.state;
-        this.positionX;
-        this.positionY;
-        this.health;
-        this.dictionary;
+        this.sprite;
+        this.health = health;
+        this.dictionary = dictionary;
+    }
+
+    receiveDamage(damage) {
+        if (typeof damage === "number") {
+            this.health -= damage;
+        }
+        else {
+            throw new Error("Error: Damage is not a number");
+        }
+    }
+
+    changeState(state) {
+        if (state === "idle" || state === "damage" || state === "attack") {
+            this.state = state;
+            this.changeSprite();
+        }
+        else {
+            throw new Error("Error: State is not a valid state");
+        }
+    }
+
+    changeSprite() {
+        //TODO: Set correct paths and set timeout function for attack and damage state
+        switch(this.state) {
+            case "idle":
+                this.sprite = "idle/sprite/path";
+                break;
+            case "damage":
+                this.sprite = "damage/sprite/path";
+                break;
+            case "attack":
+                this.sprite = "attack/sprite/path";
+                break;
+            default:
+                throw new Error("Error: State should not be undefined");
+        }
     }
 }
