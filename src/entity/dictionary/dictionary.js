c09c5aed2befc79c30410747fc77c55ab8be6535
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-09-18 19:00:23 +0200

    Add Meaning Class

--- src/entity/dictionary/dictionary.js
+++ src/entity/dictionary/dictionary.js
@@ -1,5 +1,7 @@
 class Dictionary {
-    constructor() {
-        
+    constructor(words) {
+        this.words = words;
     }
+
+    
 }
---
+++ src/entity/dictionary/words/meaning/meaning.js
@@ -0,0 +1,8 @@
+class Meaning {
+    constructor(meaning, effect, damage, sprite) {
+        this.meaning = meaning;
+        this.effect = effect;
+        this.damage = damage;
+        this.sprite = sprite;
+    }
+}
